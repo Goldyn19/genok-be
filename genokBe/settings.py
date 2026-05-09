14ee486cf3796ccc3e52a3290166c754570717fe
Goldyn19 <nwaezegolden2002@gmail.com>
2026-05-09 01:03:44 +0100

    modified middleware to priotize Cors

--- genokBe/settings.py
+++ genokBe/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'members.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 
